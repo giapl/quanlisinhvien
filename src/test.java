96c02ce85260977f35feddbab1488f079aa1be6d
giapl <ledanggiap1704@gmail.com>
2023-05-14 13:17:54 +0700

    qq

--- src/test.java
+++ src/test.java
@@ -5,7 +5,6 @@ public class test {
         lopSV l=new lopSV("cmtt a","khtn va cn");
         sinhVien sv=new sinhVien(222001442,"le dang giap",n,8.53,l);
         sinhVien sv1=new sinhVien(22222,"le thi hong ngoc",n1,8,l);
-        l.setTenLop("a11");
         System.out.println("hien thi ten khoa:"+l.getTenLop());
         System.out.println("hien thi qua diem tb:"+sv.kiemtra());
         System.out.println("hien thi co hs nao trung ngay khong:"+sv.kiemtrasinhvien(sv1));
